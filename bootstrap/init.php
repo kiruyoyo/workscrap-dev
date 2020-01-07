57c4e72e16e357ed244c560dacb0aff6c57273de
kiruyoyo <kiruharan15@gmail.com>
2020-01-08 00:09:06 +0530

    yes

---
+++ bootstrap/init.php
@@ -0,0 +1,5 @@
+<?php
+if (!isset($_SESSION))session_start();
+
+//load  env
+require_once __DIR__.'/../app/config/_env.php';
--- public/index.php
+++ public/index.php
@@ -1,1 +1,8 @@
-hello to workscrap
+<?php
+
+require_once __DIR__ . '/../bootstrap/init.php';
+
+$app_name=getenv('APP_Name');
+
+echo  $app_name;
+#use Illuminate\Database\Capsule\Manager as Capsule;
